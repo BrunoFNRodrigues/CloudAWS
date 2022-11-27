b063770575075bcfc049f5be6cb7efe2aa96960d
BrunoFNRodrigues <bf17669@gmail.com>
2022-11-26 23:58:51 -0300

    f

--- main.tf
+++ main.tf
@@ -64,7 +64,7 @@ resource "tls_private_key" "rsa" {
 
 resource "local_file" "tf-key" {
     content  = tls_private_key.rsa.private_key_pem
-    filename = "../keys/instance_kp"
+    filename = "instance_kp"
 }
 
 #Cria instancia
